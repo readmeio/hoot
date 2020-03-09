f55e37a680cd1873591aba34b6d6e8884388ad02
Kanad Gupta <kgupta@umn.edu>
2020-03-09 17:23:20 -0500

    Consolidate readme project auth with jwt.js
    The separation between the ReadMe API and the JWT was good for readability, but it felt a little too confusing and tacking something onto the req object felt unnecessary when it was only being used once.
    
    I also added a bunch of comments explaining each part.

--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,9 @@
 const proxy = require('./proxy');
-const readme = require('./readme');
 const authorization = require('./authorization');
 const jwt = require('./jwt');
 const metrics = require('./metrics');
 
 module.exports = function(app) {
-  readme(app);
   proxy(app);
   authorization(app);
   jwt(app);
--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,22 +1,44 @@
 const { sign } = require('jsonwebtoken');
+const tiny = require('tiny-json-http');
+
+const url = 'https://dash.readme.io/api/v1/';
+const apiKey = process.env.API_KEY;
 
 module.exports = function(app) {
   app.use(async (req, res, next) => {
+    // We first use the Readme API to obtain ReadMe project metadata
+    // such as the project JWT secret and base URL
+    // See the docs: https://docs.readme.com/developers/reference/projects#getproject
+    // You can also view the the JWT Secret in ReadMe by going to
+    // your ReadMe project dashboard and navigating to: Configuration -> Custom Login
+    const headers = { authorization: `Basic ${Buffer.from(`${apiKey}:`).toString('base64')}` };
+    let response;
+
+    try {
+      response = await tiny.get({ url, headers });
+    } catch (err) {
+      return res.status(401).json({ error: 'Oops! Looks like the ReadMe API key is invalid.' });
+    }
+
+    const project = response.body;
+
+    // This is the user payload that will be securely sent to ReadMe via JWT!
+    // You can read more about JWT here: https://jwt.io
     const user = {
-      // id is used to show logs in the documentation! See: https://docs.readme.com/metrics/docs/showing-api-logs-to-users#section-enabling-api-logs-in-docs
+      // id is used to identify the user and show their logs in the documentation!
+      // See the docs: https://docs.readme.com/metrics/docs/showing-api-logs-to-users#section-enabling-api-logs-in-docs
       id: req.user,
       name: req.user,
       apiKey: req.user,
-      // basicAuth is used to log into API Explorer! See: https://docs.readme.com/guides/docs/passing-data-to-jwt#section-basic-auth
+      // basicAuth is used to log into API Explorer!
+      // See the docs: https://docs.readme.com/guides/docs/passing-data-to-jwt#section-basic-auth
       basicAuth: { user: req.user, pass: '' },
     };
 
-    // The JWT secret was retrieved using the ReadMe API (see `middleware/readme.js`)
-    // You can also view the the JWT Secret in ReadMe by going to
-    // your ReadMe project dashboard and navigating to: Configuration -> Custom Login
+    const jwt = sign(user, project.jwtSecret);
 
-    const jwt = sign(user, req.project.jwtSecret);
-    const docsUrl = `${req.project.baseUrl}reference`;
+    // These local variables are the links that automatically log users into the documentation
+    const docsUrl = `${project.baseUrl}reference`;
     res.locals.docsUrl = docsUrl;
     res.locals.jwt = `${docsUrl}?auth_token=${jwt}`;
 
@@ -29,6 +51,6 @@ module.exports = function(app) {
       res.set('x-readme-email', req.user);
     }
 
-    next();
+    return next();
   });
 };
