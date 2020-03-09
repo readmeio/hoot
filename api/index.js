1831b588972d354c31bb3bdd4782bef67b297b24
Kanad Gupta <kgupta@umn.edu>
2020-03-09 17:20:57 -0500

    Eh...
    After thinking about it more, moving from the dynamic require back to the static require. It's easier to read and better for a demonstrative repo!

--- api/index.js
+++ api/index.js
@@ -1,14 +1,6 @@
 const router = require('express').Router();
-const fs = require('fs');
 
-fs.readdirSync(__dirname).forEach(file => {
-  // Do not attempt to require non-js files
-  if (!file.match(/.js$/) || file === 'index.js') return;
-
-  const fileName = file.split('.')[0];
-
-  // eslint-disable-next-line import/no-dynamic-require, global-require
-  router.use(`/${fileName}`, require(`${__dirname}/${file}`));
-});
+router.use('/hoot', require('./hoot'));
+router.use('/timeline', require('./timeline'));
 
 module.exports = router;
