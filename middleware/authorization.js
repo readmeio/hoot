fcc7c72b56cd17e60b67503b785be272c1a9466c
Kanad Gupta <kgupta@umn.edu>
2020-03-09 17:23:59 -0500

    Adding more comments

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -2,6 +2,8 @@ const atob = require('atob');
 
 module.exports = function(app) {
   app.use((req, res, next) => {
+    // Authorization headers are used to authenticate requests from the HTTP API
+    // Basic Auth is also how we log users into the documentation! See middlewares/jwt.js
     if (req.headers.authorization) {
       try {
         const b64 = req.headers.authorization.split(' ')[1];
@@ -9,6 +11,9 @@ module.exports = function(app) {
         req.user = user;
       } catch (e) {} // eslint-disable-line no-empty
     } else {
+      // Otherwise if the user is just on the website, we just look for their username cookie
+      // This is purely for demonstrative purposes!
+      // We do not recommend performing actual authentication this easily!
       req.user = req.cookies.username;
     }
     res.locals.user = req.user;
--- middleware/metrics.js
+++ middleware/metrics.js
@@ -1,9 +1,16 @@
 const readmeio = require('readmeio');
 
+// This is the middleware for sending request and response data to Readme's API Metrics!
+// See the docs: https://docs.readme.com/metrics/docs/sending-logs-to-readme-with-nodejs
+
 module.exports = function(app) {
   app.use(
     '/api/*',
     readmeio.metrics(process.env.API_KEY, req => ({
+      // In order to let the user view their own logs in the documentation,
+      // The id below must correspond to the 'id' that's passed in via JWT
+      // when users log into the documentation. See middlewares/jwt.js
+      // as well as the documentation: https://docs.readme.com/metrics/docs/showing-api-logs-to-users#section-enabling-api-logs-in-docs
       id: req.user,
       label: req.user,
       email: req.user,
