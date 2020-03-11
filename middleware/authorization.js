2a329b30dee34346b0bc638cd0ad992e5abaa1f2
Kanad Gupta <kgupta@umn.edu>
2020-03-11 14:38:44 -0500

    Update comments and flow in auth and jwt files

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,8 +1,15 @@
 const atob = require('atob');
 
+// DISCLAIMER: This authorization flow is simplified is purely for demonstrative purposes!
+// We do not recommend performing actual authentication like this!
+
+// For simplicity's sake, we log users into Hoot with just a username and no password.
+// When users log in, we simply store their username in a cookie — see $('#login').submit() in public/js/hoot.at.js
+// When users log out, we simply clear the cookie — see the /logout route in index.js
+
 module.exports = (req, res, next) => {
   // Authorization headers are used to authenticate requests from the HTTP API
-  // Basic Auth is also how we log users into the documentation! See middleware/jwt.js
+  // We also pass users' Basic Auth credentials into the API docs via Custom Login! See middleware/jwt.js
   if (req.headers.authorization) {
     try {
       const b64 = req.headers.authorization.split(' ')[1];
@@ -10,9 +17,8 @@ module.exports = (req, res, next) => {
       req.user = user;
     } catch (e) {} // eslint-disable-line no-empty
   } else {
-    // Otherwise if the user is just on the website, we just look for their username cookie
-    // This is purely for demonstrative purposes!
-    // We do not recommend performing actual authentication this easily!
+    // When the users visit the site, we look for the 'username' cookie.
+    // If we find the cookie, we log them in!
     req.user = req.cookies.username;
   }
   res.locals.user = req.user;
--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,31 +1,38 @@
 const { sign } = require('jsonwebtoken');
 
+// This middleware demonstrates how to use Hoot login credentials to log users
+// into the Hoot ReadMe documentation using Custom Login and JWT (JSON Web Token)
+// Read our docs to learn more: https://docs.readme.com/guides/docs/custom-login-with-readme
+
 module.exports = async (req, res, next) => {
-  // set in bin/readme.js
+  // The ReadMe project data is obtained via API in bin/readme.js
   const { project } = req.app.settings;
 
-  // This is the user payload that will be securely sent to ReadMe via JWT!
-  // You can read more about JWT here: https://jwt.io
-  const user = {
-    // id is used to identify the user and show their logs in the documentation!
-    // See the docs: https://docs.readme.com/metrics/docs/showing-api-logs-to-users#section-enabling-api-logs-in-docs
-    id: req.user,
-    name: req.user,
-    apiKey: req.user,
-    // basicAuth is used to log into API Explorer!
-    // See the docs: https://docs.readme.com/guides/docs/passing-data-to-jwt#section-basic-auth
-    basicAuth: { user: req.user, pass: '' },
-  };
-
-  const jwt = sign(user, project.jwtSecret);
-
-  // These local variables are the links that automatically log users into the documentation
+  // If the user is not logged in, we give them a standard link to the API Docs (see public/views/template.pug)
   const docsUrl = `${project.baseUrl}reference`;
   res.locals.docsUrl = docsUrl;
-  res.locals.jwt = `${docsUrl}?auth_token=${jwt}`;
 
   if (req.user) {
-    // If you're connecting to metrics via our Cloudflare worker,
+    // The user is logged in, so we construct the JWT payload. This is sent to ReadMe to log them into the docs!
+    // You can read more about JWT (JSON Web Token) here: https://jwt.io
+    const user = {
+      // The 'id' field below is used in the payload to identify the user and show their logs in the documentation!
+      // See the docs: https://docs.readme.com/metrics/docs/showing-api-logs-to-users#section-enabling-api-logs-in-docs
+      id: req.user,
+      name: req.user,
+      apiKey: req.user,
+      // The basicAuth object below contains the 'user' and 'pass' fields needed to log the user into API Explorer!
+      // See the docs: https://docs.readme.com/guides/docs/passing-data-to-jwt#section-basic-auth
+      basicAuth: { user: req.user, pass: '' },
+    };
+
+    const jwt = sign(user, project.jwtSecret);
+
+    // Instead of giving the user the standard link to the API docs we created in res.locals.docsUrl,
+    // we construct a "magic" link that contains the JWT. This link will automatically log the user into the docs!
+    res.locals.jwt = `${docsUrl}?auth_token=${jwt}`;
+
+    // If you're connecting to API Metrics via our Cloudflare worker,
     // you'll want to set response headers as shown below.
     // More info: https://docs.readme.com/metrics/docs/sending-logs-to-readme-with-cloudflare
     res.set('x-readme-id', req.user);
